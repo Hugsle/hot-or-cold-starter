8ad34bf8369312193906d31ad5db928bc74927d0
Jack C. <popjack4321@gmail.com>
2014-10-09 16:38:08 -0700

    capture input

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+$(document).ready(function() {
 
-  	});
+    /*--- Display information modal box ---*/
+    $(".what").click(function() {
+        $(".overlay").fadeIn(1000);
+    
+    });
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
+    /*--- Hide information modal box ---*/
+    $("a.close").click(function() {
+        $(".overlay").fadeOut(1000);
+    });
 
 });
 
@@ -17,13 +17,30 @@ $(document).ready(function(){
 function newGame() {
 
 }
+/*---Generate Number---*/
 
-function genNum(num) {
-	var num = Math.floor((Math.random() * 100) + 1);
-	return num;
-}
-genNum();
+        var genNum = Math.floor((Math.random() * 100) + 1);
+        console.log("Generated Random Number = " + genNum);
 
-function userFeedback() {
-	
+/*---Capture/Evaluate Input---*/
+    function userInput() {
+        $('#guessButton').on('click', function(e) {
+            e.preventDefault();
+            var input = $("#userGuess").val();
+            console.log("The input was " + input);
+        if (input > genNum){
+            console.log("Higher");
+        } else if (input < genNum) {
+            console.log("Lower");
+        } else if (input == genNum) {
+            console.log("You got it!");
+        }
+        ;
+        });
+    }
+    ;
+    userInput();
+
+function inputEval() {
+
 }
