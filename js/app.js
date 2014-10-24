229dc1773678df2f2c84d99ff85d9a82a8d03889
Jack C. <popjack4321@gmail.com>
2014-10-23 18:21:53 -0700

    blah

--- js/app.js
+++ js/app.js
@@ -10,15 +10,17 @@ $(document).ready(function() {
     $("a.close").click(function() {
         $(".overlay").fadeOut(1000);
     });
-    /*---Display Invaid Value Text---*/
+    /*---Display Invaid Value Text---
     if (input < 1 && input > 100){
           $("form").show("<div>Please enter a number between 1 to 100!</div>");
-        }
+        }*/
 
 /*----Refresh Page---*/
 function newGame() {
   $('.new').on('click', function() {
-    location.reload();
+    counter = 0;
+    num = generateNumber();
+    guesses = [];
   });
 }
 /*---Generate Number---*/
@@ -33,26 +35,35 @@ function generateNumber() {
 
 /*---Capture/Evaluate Input---*/
     function userInput() {
-
+      var counter = 0;
+      $('#count').text(counter);
       var input = null;
       var num = generateNumber();
       var guesses = [];
         $('#guessButton').on('click', function(e) {
             e.preventDefault();
+            counter +=1;
+            $('#count').text(counter);
+            console.log(counter);
             input = $("#userGuess").val();
             console.log("The input was " + input);
             guesses.push(input);
-            $('#guesslist').append("<li>" + guesses + "</li>");
+            $('#guessList').append("<li>" + input + "</li>");
             console.log(guesses);
 
-        if (input > num){
-            console.log("Higher");
-        } else if (input < num) {
-            console.log("Lower");
-        } else if (input == num) {
+        if (input == num){
+          $('#feedback').text("You got it!");
             console.log("You got it!");
-        }
-        ;
+        } else if ((input - num) <= 15 && (input - num) > 0) {
+          $('#feedback').text("Flaming Hot!");
+            console.log("Hotter");
+        } else if ((num - input) <= 15 && (num - input) > 0) {
+          $('#feedback').text("Blistering Cold!");
+            console.log("Colder");
+        } else if ((input - num) <= 30 && (input - num) > 0) {
+          $('#feedback').text("Getting Hotter!");
+        } else if ((num - input) <= 30 && (num - input) > 0)
+        ; $('#feedback').text("Getting Colder!");
         });
     }
     ;
